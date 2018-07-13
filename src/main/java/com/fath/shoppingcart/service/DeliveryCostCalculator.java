1e09aaa8a4d4598048c52a3982155dbe949feab5
fatih.senturk <fatih.senturk@n11.com>
2018-07-13 10:34:57 +0300

    added calculator logic

--- src/main/java/com/fath/shoppingcart/service/DeliveryCostCalculator.java
+++ src/main/java/com/fath/shoppingcart/service/DeliveryCostCalculator.java
@@ -2,12 +2,13 @@ package com.fath.shoppingcart.service;
 
 import com.fath.shoppingcart.dto.CartDto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class DeliveryCostCalculator {
 
     private Double costPerDelivery;
-    private Integer numberOfDeliveries;
     private Double costPerProduct;
-    private Integer numberOfProducts;
     private Double fixedCost = 2.99;
 
     public DeliveryCostCalculator(Double costPerDelivery, Double costPerProduct, Double fixedCost) {
@@ -17,7 +18,19 @@ public final class DeliveryCostCalculator {
     }
 
     public double calculateFor(CartDto cart) {
-        return 0;
+        int numberOfDeliveries = this.getNumberOfDeliveries(cart);
+        int numberOfProduct = this.getNumberOfProducts(cart);
+        return (costPerDelivery * numberOfDeliveries) + (costPerProduct * numberOfProduct) + fixedCost;
+    }
+
+    private int getNumberOfProducts(CartDto cart) {
+        return cart.getProducts().size();
+    }
+
+    private int getNumberOfDeliveries(CartDto cart) {
+        Set<String> categoryIds = new HashSet<>();
+        cart.getProducts().forEach(productDto -> categoryIds.add(productDto.getCategoryId()));
+        return categoryIds.size();
     }
 
 }
